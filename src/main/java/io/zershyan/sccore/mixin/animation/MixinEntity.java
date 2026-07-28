4133a122ab15fd015a6e3bd51a7ac5eff4b89e8a
Zershyan <1283411677@qq.com>
2026-07-28 19:54:56 +0800

    1.0.7-alpha1

--- src/main/java/io/zershyan/sccore/mixin/animation/MixinEntity.java
+++ src/main/java/io/zershyan/sccore/mixin/animation/MixinEntity.java
@@ -79,6 +79,7 @@ public abstract class MixinEntity {
             if(heightModifier == 1.0f) return;
             setPose(Pose.STANDING);
             cir.setReturnValue(Pose.STANDING);
+            cir.cancel();
         }
     }
 }
